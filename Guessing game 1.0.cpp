c9ff9b76f586f81d86d517d1ff5d1c7b0a9d6d95
ParentalUnit1of2 <90790291+ParentalUnit1of2@users.noreply.github.com>
2021-12-22 22:35:12 -0600

    Add project files.

---
+++ Guessing game 1.0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int player1, player2, counter;
+	counter = 1;
+	cout << "Can you guess the first players number?" << endl;
+	cout << "\nLets play! \n \nPlayer 1 please enter a number: ";
+	cin >> player1;
+	system("cls");
+
+	cout << "Can you guess the first players number?" << endl;
+	cout <<  "\nPlayer 2 please enter your guess: ";
+	cin >> player2;
+
+   
+
+
+}
+
